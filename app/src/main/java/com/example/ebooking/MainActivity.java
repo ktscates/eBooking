4d1e4f4d2864d5298ced33e0ca225691c965e8bb
ktscates <cates2506@gmail.com>
2019-10-11 20:39:22 +0200

    First commit, login landing page

---
+++ app/src/main/java/com/example/ebooking/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ebooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.user_edit_text);
+        password = (EditText) findViewById(R.id.pass_edit_text);
+        login = (Button) findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+//                Intent optionPage = new Intent(MainActivity.this, OptionsActivity.class);
+//                optionPage.putExtra("user", user);
+//                startActivity(optionPage);
+            }
+        });
+    }
+}
