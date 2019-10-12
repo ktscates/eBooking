5721ee3b48adb231c653889883464eb13bb32325
ktscates <cates2506@gmail.com>
2019-10-12 03:09:21 +0200

    Changes BusActivity class

--- app/src/main/java/com/example/ebooking/BusActivity.java
+++ app/src/main/java/com/example/ebooking/BusActivity.java
@@ -1,4 +1,15 @@
 package com.example.ebooking;
 
-public class BusActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bus_activity);
+
+    }
 }
