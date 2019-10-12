58c1f11a54016caa03b6d97140154ac9e52adace
ktscates <cates2506@gmail.com>
2019-10-12 02:19:40 +0200

    Add bus activities for booking

---
+++ app/src/main/java/com/example/ebooking/BusActivity.java
@@ -0,0 +1,4 @@
+package com.example.ebooking;
+
+public class BusActivity {
+}
--- app/src/main/java/com/example/ebooking/OptionsActivity.java
+++ app/src/main/java/com/example/ebooking/OptionsActivity.java
@@ -1,16 +1,26 @@
 package com.example.ebooking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OptionsActivity extends AppCompatActivity {
 
+    private Button busTicket;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.options_activity);
 
+        busTicket = (Button) findViewById(R.id.bus_button);
+    }
 
+    public void busButton(View view){
+        Intent busPage= new Intent(OptionsActivity.this, BusActivity.class);
+        startActivity(busPage);
     }
 }
