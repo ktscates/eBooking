6abad90e0b517569e55df25a04b17ec40184ad3a
ktscates <cates2506@gmail.com>
2019-10-12 03:31:33 +0200

    Create xml actvities for class activities

---
+++ app/src/main/java/com/example/ebooking/RestoActivity.java
@@ -0,0 +1,15 @@
+package com.example.ebooking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resto_activity);
+
+    }
+}
