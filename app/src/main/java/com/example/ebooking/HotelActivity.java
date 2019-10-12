c71133088a6355588fc10edad2d29d72d1c99840
ktscates <cates2506@gmail.com>
2019-10-12 21:42:30 +0200

    Change hotel xml and hotel class

--- app/src/main/java/com/example/ebooking/HotelActivity.java
+++ app/src/main/java/com/example/ebooking/HotelActivity.java
@@ -1,8 +1,11 @@
 package com.example.ebooking;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class HotelActivity extends AppCompatActivity {
 
@@ -10,6 +13,5 @@ public class HotelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hotel_activity);
-
     }
 }
